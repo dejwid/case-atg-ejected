7152f620503d8bca3e1530a0d81239b818bf2566
dejwid <dawid.paszko@gmail.com>
2023-10-19 15:24:45 +0200

    Tar bort oanvand useEffect

--- src/CollapsiblePanel.js
+++ src/CollapsiblePanel.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export default function CollapsiblePanel({labelLeft, labelRight, children, onOpen, onClose, isLoading, size='xl'}) {
   const [isOpen, setIsOpen] = useState(false);
